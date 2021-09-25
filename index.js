c7d1e91d0f3d07ba9fac6de2a2a0863e10fcfb6b
Leon <hualiang.tian@gmail.com>
2021-09-24 21:14:02 -0400

    commit tree 166816b5af5b74351c235d88264be1267253b5cf

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+function map(ary,fctn)
+{
+    const new1 = [];
+    for (let i = 0; i < ary.length; i++)
+    {
+        const emt = ary[i];
+        new1.push(fctn(emt));
+    }
+    return new1;
+}
+
+function reduce(ary, fctn, init)
+{
+    let compiled;
+    if (init)
+    {
+        compiled = init;
+        for (let i = 0; i < ary.length; i++)
+        {
+            compiled = fctn(ary[i],compiled);
+        }
+        return compiled;
+    }
+    else
+    {
+        compiled = ary[0];
+        for (let i = 1; i < ary.length; i++)
+        {
+            compiled = fctn(ary[i],compiled);
+        }
+        return compiled;
+    }
+}
